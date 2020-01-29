4c602764645146d06b0210a101164e944532a053
Singh, Harminder <rednimrah20@gmail.com>
2020-01-29 12:15:51 +0530

    Creating View Controllers and CoreData entities

---
+++ HarminderMobileDevTest/ComapanyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ComapanyTableViewCell.swift
+//  HarminderMobileDevTest
+//
+//  Created by Singh, Harminder (external - Project) on 29/01/20.
+//  Copyright © 2020 Singh, Harminder. All rights reserved.
+//
+
+import UIKit
+
+class ComapanyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ HarminderMobileDevTest/MemberViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemberViewController.swift
+//  HarminderMobileDevTest
+//
+//  Created by Singh, Harminder (external - Project) on 29/01/20.
+//  Copyright © 2020 Singh, Harminder. All rights reserved.
+//
+
+import UIKit
+
+class MemberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
