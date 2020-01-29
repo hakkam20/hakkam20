0af160d84be849b29f93aac614d4644e070ecedd
Singh, Harminder (external - Project) <harminder.singh03@sap.com>
2020-01-29 11:08:50 +0530

    Initial Commit

---
+++ HarminderMobileDevTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HarminderMobileDevTest
+//
+//  Created by Singh, Harminder (external - Project) on 29/01/20.
+//  Copyright © 2020 Singh, Harminder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
